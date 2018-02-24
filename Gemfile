c38f970ceefccd7126aebf6799f67dd7f10e231b
Yoji Okada <yokada.moo.jp@gmail.com>
2018-02-25 02:27:38 +0900

    init

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'asciidoctor'
+gem 'asciidoctor-diagram'
+gem 'asciidoctor-pdf'
+gem 'asciidoctor-pdf-cjk'
+gem 'coderay'
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    bsync = require('browser-sync'),
+    exec = require('child_process').exec;
+
+gulp.task('browser-sync', function() {
+    bsync({
+        server: {
+            baseDir: "./docs/html/",
+            index  : "./docs/html/README.html"
+        }
+    });
+});
+
+gulp.task('asciidoc', function(cb) {
+    return exec('bundle exec asciidoctor -D ./docs/html -r asciidoctor-diagram docs/*.adoc', function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('reload', function () {
+    bsync.reload();
+});
+
+gulp.task('default', ['asciidoc', 'browser-sync'], function(cb) {
+    gulp.watch('./**/*.adoc', ['asciidoc']);
+    gulp.watch('./**/*.html', ['reload']);
+});
